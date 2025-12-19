56be8308cfc0d2a974dc9247a4dab0421bba7f22
Ken Jin <kenjin4096@gmail.com>
2025-12-19 14:56:45 +0000

    fix icmp bug

--- ykrt/src/compile/j2/opt/known_bits.rs
+++ ykrt/src/compile/j2/opt/known_bits.rs
@@ -66,11 +66,6 @@ impl KnownBitValue {
         }
     }
 
-    /// Check self is equivalent to other in terms of known bits.
-    fn equiv(&self, other: &KnownBitValue) -> bool {
-        self.ones == other.ones && self.unknowns == other.unknowns
-    }
-
     /// CHeck self is contained by the other in terms of known bits.
     /// In other words, other contains more information than self.
     fn contained_by(&self, other: &KnownBitValue) -> bool {
@@ -224,13 +219,15 @@ fn opt_icmp(opt: &mut Opt, mut inst: ICmp) -> OptOutcome {
     let lhs_b = opt.as_known_bits(lhs, opt.inst_bitw(opt, lhs));
     let rhs_b = opt.as_known_bits(rhs, opt.inst_bitw(opt, rhs));
     match pred {
-        IPred::Eq if rhs_b.all_known() => {
+        // We can only deduce if match. We can't deduce
+        // anything if they don't.
+        IPred::Eq if rhs_b.all_known() && rhs_b.contained_by(&lhs_b) => {
             let tyidx = opt.push_ty(Ty::Int(1)).unwrap();
-            let b = rhs_b.contained_by(&lhs_b) as u64;
-            opt.set_known_bits(KnownBitValue::from_constant(&ArbBitInt::from_u64(1, b)));
+            let one = ArbBitInt::from_u64(1, 1);
+            opt.set_known_bits(KnownBitValue::from_constant(&one));
             return OptOutcome::Rewritten(Inst::Const(Const {
                 tyidx,
-                kind: ConstKind::Int(ArbBitInt::from_u64(1, b)),
+                kind: ConstKind::Int(one),
             }))
         }
         _ => OptOutcome::Rewritten(inst.into())
