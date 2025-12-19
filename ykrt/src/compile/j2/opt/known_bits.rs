e8336541d2bdd875775db011432b852b2c64f8a3
Ken Jin <kenjin4096@gmail.com>
2025-12-19 16:29:49 +0000

    re-enable icmp

--- ykrt/src/compile/j2/opt/known_bits.rs
+++ ykrt/src/compile/j2/opt/known_bits.rs
@@ -211,20 +211,20 @@ fn opt_icmp(opt: &mut Opt, mut inst: ICmp) -> OptOutcome {
         samesign,
     } = inst;
     assert!(!samesign);
-    // let lhs_b = opt.as_known_bits(lhs, opt.inst_bitw(opt, lhs));
-    // let rhs_b = opt.as_known_bits(rhs, opt.inst_bitw(opt, rhs));
-    // if rhs_b.all_known() && lhs_b.all_known() {
-    //     if let IPred::Eq = pred {
-    //         let b = rhs_b.contained_by(&lhs_b);
-    //         let tyidx = opt.push_ty(Ty::Int(b as u32)).unwrap();
-    //         let b_as_int = ArbBitInt::from_u64(1, b as u64);
-    //         opt.set_known_bits(KnownBitValue::from_constant(&b_as_int));
-    //         return OptOutcome::Rewritten(Inst::Const(Const {
-    //             tyidx,
-    //             kind: ConstKind::Int(b_as_int),
-    //         }))
-    //     }
-    // }
+    let lhs_b = opt.as_known_bits(lhs, opt.inst_bitw(opt, lhs));
+    let rhs_b = opt.as_known_bits(rhs, opt.inst_bitw(opt, rhs));
+    if rhs_b.all_known() && lhs_b.all_known() {
+        if let IPred::Eq = pred {
+            let b = rhs_b.contained_by(&lhs_b);
+            let tyidx = opt.push_ty(Ty::Int(b as u32)).unwrap();
+            let b_as_int = ArbBitInt::from_u64(1, b as u64);
+            opt.set_known_bits(KnownBitValue::from_constant(&b_as_int));
+            return OptOutcome::Rewritten(Inst::Const(Const {
+                tyidx,
+                kind: ConstKind::Int(b_as_int),
+            }))
+        }
+    }
     OptOutcome::Rewritten(inst.into())
 }
 
